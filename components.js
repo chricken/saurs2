e308e975c28bf5a124c1b34fbc4376fde899fe28
Chricken <post@beardedbox.com>
2022-03-13 08:10:55 +0100

    kleine Änderung im CSS

--- components.js
+++ components.js
@@ -1,16 +1,17 @@
-
 import ui from "./ui.js";
 import dom from './dom.js';;
 
 const components = {
     details(data) {
         ui.elDetails.innerHTML = '';
+
         let elBez = dom.create({
             type: 'h3',
             content: data.bez,
             parent: ui.elDetails
         })
-    }
+    },
+
 }
 
 export default components;
