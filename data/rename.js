c982ff7fadb633967bb0cd14cd64b4a6120f0f13
Chricken <post@beardedbox.com>
2022-03-17 08:19:53 +0100

    Anpassungen der Locations

--- data/rename.js
+++ data/rename.js
@@ -1,28 +1,63 @@
 'use strict';
 
+const { log } = require('console');
 const fs = require('fs');
 
 let data = false;
-let locations = [];
+let locations = new Set();
+
+let suchen = ['Neixiang'];
+let ersetzen = 'chinamitte';
+
+let count = 0;
 
 const extractLocations = ast => {
-    console.log(ast);
+    for (let key in ast) {
+        count++;
+        let val = ast[key];
+        if (val.fundort) {
+
+            // Suchen und ersetzen
+            suchen.forEach(suche => {
+
+                val.fundort = val.fundort.split(suche).join(ersetzen);
+            })
+
+            // Locations-Übersicht füllen
+            val.fundort.split(', ').forEach(fundort => locations.add(fundort));
+
+            // Ausgabe
+            /*
+            console.log(key, count);
+            console.log(val.fundort);
+            console.log(' ');
+            */
+        }
+        if (val.children) extractLocations(val.children);
+    }
 }
 
 
 const init = () => {
-    fs.readFile('./daten.json')
 
-    /*
-    .then(
-        (res) => {
-            data = JSON.parse(res.toString());
+    fs.readFile('./daten.json', (err, res) => {
+        if (err) console.log(err);
+        else {
+            data = JSON.parse(res);
             extractLocations(data);
+            console.log('Count: ', count);
+            console.log([...locations].join(', '));
+
+            fs.writeFile(
+                './daten.json',
+                JSON.stringify(data),
+                (err, res) => {
+                    if (err) console.log(err);
+                    else console.log('done');
+                }
+            )
         }
-    ).catch(
-        console.log
-    )
-    */
+    })
 }
 
 init();
